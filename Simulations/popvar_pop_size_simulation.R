a629631cbd78b143d4442772e6661b6d0d9f753d
neyhartj <neyhartje@gmail.com>
2017-11-03 13:10:01 -0500

    Added many scripts and supporting data

--- Simulations/popvar_pop_size_simulation.R
+++ Simulations/popvar_pop_size_simulation.R
@@ -1,60 +1,191 @@
 ## PopVar Size Simulations
+## 
+## This script will run some simulations to determine the effect of various 
+## perturbations on the ability to statistically differentiate genetic variance 
+## in different populations
+## 
 
-library(tidyverse)
-library(stringr)
-library(PopVar)
-library(qtl)
-library(pbsim)
-library(simcross)
-library(lme4)
-library(parallel)
+packages <- c("dplyr", "tidyr", "tibble", "purrr", "readr", "stringr", "modelr", 
+              "pbsim", "pbr", "purrrlyr", "lme4", "parallel", "gws")
+
+# Set the directory of the R packages
+package_dir <- NULL
+package_dir <- "/panfs/roc/groups/6/smithkp/neyha001/R/x86_64-pc-linux-gnu-library/3.4/"
+
+# Load all packages
+invisible(lapply(packages, library, character.only = TRUE, lib.loc = package_dir))
 
 
 proj_dir <- "C:/Users/Jeff/Google Drive/Barley Lab/Projects/PopVarVal"
+proj_dir <- "/panfs/roc/groups/6/smithkp/neyha001/Genomic_Selection/PopVarVal"
 
 sim_dir <- file.path(proj_dir, "Simulations")
 
+# Load the S6 data
+data(s6_cap_haploid)
+data(s6_snp_info)
 
-data("s2_genos")
-data("s2_snp_info")
+
 
 
 ## Immutable parameter 
 # Number of simulation iterations
-n_iter <- 50
-
+n_iter <- 5
 # Number of replicates in environments
-n_rep <- 2
-
+n_rep <- 3
 # Number of environments to phenotype
 n_env <- 4
-
-# Error variance for genetic map positions
-map_error <- 0.002
-
 # Number of bi-parental populations
 n_cross <- 20
+# Number of selfing generations
+self_gen <- 2
+# Selection intensity for superior progeny calculation
+i_sp <- 0.10 
+
+# Define the biologically "real" map
+real_map <- s6_snp_info %>% 
+  split(.$chrom) %>% 
+  map(~ set_names(x = .$cM_pos, nm = .$rs)) %>%
+  structure(class = "map")
+
+# Create the real genome
+genome <- sim_genome(map = real_map, type = "hypred")
 
 
 ## Mutable parameters
+# Error variance for genetic map positions
+map_error <- c(1, 5, 10)
 # Heritability
 herit <- c(0.2, 0.5, 0.8)
-
 # Number of QTL
 nQTL <- c(30, 100)
-
 # Number of individuals in the bi-parental family
-n_ind <- c(25, 75, 125, 200, 275)
+n_ind <- c(25, 100, 500)
 
 # Create a data.frame of the mutable parameters to iterate over
-param_df <- expand.grid(herit = herit, nQTL = nQTL, n_ind = n_ind, iter = seq(n_iter))
+param_df <- expand.grid(iter = seq(n_iter), map_error = map_error, herit = herit, 
+                        nQTL = nQTL, n_ind = n_ind)
+
+# The maximum number of qtl is the max of the nQTL vector
+max_qtl <- max(nQTL)
+
 
 # Detect cores
 n_cores <- detectCores()
 
-# Split the data.frame
-df_split <- split(x = param_df, f = sort(rep_len(x = seq(n_cores), length.out = nrow(param_df))))
+# Add a column of cores and split the data.frame
+param_df %>% 
+  mutate(core = sort(rep(seq(n_cores), length.out = nrow(.)))) %>%
+  split(.$core) %>%
+  mclapply(function(core_df) {
+    
+    # Apply by row
+    core_df %>%
+      by_row(function(i) {
+        
+        ## Variables
+        map_error <- i$map_error
+        herit <- i$herit
+        n_qTL <- i$nQTL
+        n_ind <- i$n_ind
+        
+        ## Create the genomes with genetic models
+        
+        # Add error to the genetic map
+        useable_map <- real_map %>% 
+          map(~ . + rnorm(n = length(.), mean = 0, sd = sqrt(map_error))) %>%
+          # Correct for negative map positions
+          map(~ . + abs(min(.))) %>%
+          map(sort) %>%
+          structure(class = "map")
+        
+        # Create the usable genome
+        genome_usable <- sim_genome(map = useable_map, type = "hypred")
+        
+        # Genetic model matrix
+        qtl.model <- matrix(nrow = n_qtl, ncol = 4)
+        
+        # Add genetic architecture
+        genome <- sim_gen_model(genome, qtl.model, add.dist = "geometric", max.qtl = max_qtl)
+        # Copy the genetic model to the useable genome
+        genome_usable$gen_model <- genome$gen_model
+        
+        # Define a pedigree for the families of selection candidates
+        pedigree <- sim_pedigree(n.ind = n_ind, n.selfgen = self_gen)
+        
+        
+        
+        ## PopVar prep
+        # Pull out marker names
+        marker_names <- markernames(genome)
+        
+        # Popvar prep for the usable genome
+        popvar_map <- map_to_popvar(genome_usable) %>%
+          filter(!marker %in% qtlnames(genome_usable))
+        
+        # Create the base population
+        training_pop <- create_pop(genome = genome, geno = s6_cap_haploid)
+        # Phenotypic observations for the training population
+        training_pop <- sim_phenoval(pop = training_pop, h2 = herit, n.env = n_env, n.rep = n_rep)
+        
+        # Select the best 50 MN and best 50 ND
+        NDnames <- indnames(training_pop) %>% 
+          str_subset("^ND")
+        MNnames <- setdiff(indnames(training_pop), NDnames)
+        
+        NDbest <- subset_pop(pop = training_pop, individual = NDnames) %>%
+          select_pop(intensity = 50, index = 1)
+        MNbest <- subset_pop(pop = training_pop, individual = MNnames) %>%
+          select_pop(intensity = 50, index = 1)
+        
+        potential_parents <- combine_pop(list(NDbest, MNbest)) %>% indnames()
 
+        
+        
+        # Extract the variance components
+        trait_var_comp <- training_pop$pheno_val$var_comp
+        
+        # Designate the training population
+        ## The 'popvar_geno_pop' is for use in PopVar (before parental selection)
+        ## The 'selection_training_pop' is for predicting the selection candidates
+        popvar_geno_pop <- training_pop
+        
+        # Use the phenotypic means as the predicted genotypic values
+        popvar_geno_pop$pred_val <- popvar_geno_pop$pheno_val$pheno_mean
+        
+        
+        ### Parental selection
+        # The maximum number of crosses given the potential parents
+        max_crosses <- choose(length(potential_parents), 2)
+        
+        # Simualte a crossing block
+        crossing_block <- sim_crossing_block(parents = potential_parents, n.crosses = max_crosses)
+        
+        # Extract the genotype data for use in the function
+        G <- genotype(genome = genome, pop = popvar_geno_pop)
+        # Phenotype data
+        phenos <- popvar_geno_pop$pheno_val$pheno_mean
+        
+        
+        sim_fam <- pop_predict_quick(G.in = G, y.in = phenos, map.in = popvar_map, 
+                                     crossing.table = crossing_block, tail.p = i_sp)
+        
+        
+        # Subset the families by those with predicted genetic variances that are
+        # less than or greater than the 10% upper and lower quantiles
+        # Then sort on the mean
+        sim_fam_subset <- sim_fam %>% 
+          filter(pred_vaxrG < quantile(pred_varG, 0.10) | pred_varG > quantile(pred_varG, 0.90)) %>%
+          arrange(pred_mu)
+        
+        
+        
+        
+        
+        
+        ## Data.frame of information to collect for each cycle and iteration
+        tracked_data <- vector("list", n_cycles)
+        
 
 
 
