5fa1fdbff5776b44a9df1360cc141a99633c375b
neyhartj <neyha001@umn.edu>
2018-09-18 10:57:01 -0500

    edits to batch script

--- Scripts/Simulations/run_simulations.sh
+++ Scripts/Simulations/run_simulations.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# # #PBS -l walltime=12:00:00,mem=22gb,nodes=1:ppn=8
-#PBS -l walltime=24:00:00,mem=62gb,nodes=1:ppn=16
+#PBS -l walltime=24:00:00,mem=22gb,nodes=1:ppn=8
+# #PBS -l walltime=12:00:00,mem=62gb,nodes=1:ppn=16
 # #PBS -N PopVar_suitability_simulation
-#PBS -N PopVar_gencor_selection_simulation
+# #PBS -N PopVar_gencor_selection_simulation
+#PBS -N PopVar_gencor_prediction_simulation
 #PBS -M neyha001@umn.edu
 #PBS -m abe
 #PBS -r n
@@ -17,7 +18,7 @@ module load R/3.5.0
 # Rscript popvar_suitability_simulation.R
 
 # For genetic correlation
-# Rscript popvar_gencor_simulation.R
+Rscript popvar_gencor_simulation.R
 
 # For genetic correlation and selection
-Rscript popvar_gencor_selection_simulation.R
+# Rscript popvar_gencor_selection_simulation.R
